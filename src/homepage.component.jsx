08983177f69fa3827bc7d22f68a1238aa6aea96e
Patrick McCreanor <pmccreanor@can-jlc-qejhd4.loycorp.local>
2020-04-02 11:40:25 -0400

    initial push

---
+++ src/homepage.component.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+       <div className="teasers-container">
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">origins</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">world cup</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">goals</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">teams</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">culture</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">europe</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">brazil</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">south america</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">dark side</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">players</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">managers</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">africa</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">media</p>
+                </div>
+            </article>
+            <article className="teaser">
+                <div className="teaser__poster">
+                    <p className="teaser__title">future</p>
+                </div>
+            </article>
+       </div>
+    </div>
+)
+
+export default HomePage;
